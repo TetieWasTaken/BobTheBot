bc8595437a79a33ed9dbb2850319cca24d6b5606
Toast <74485845+ScorchedToast@users.noreply.github.com>
2022-11-14 21:02:32 +0100

    Updated serverinfo command

--- commands/Information/serverinfo.js
+++ commands/Information/serverinfo.js
@@ -1,43 +1,52 @@
-const { SlashCommandBuilder } = require('@discordjs/builders')
-const { EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('serverinfo')
-        .setDescription('Receive information about the current guild'),
-    async execute(interaction) {
-        var serverIcon = interaction.guild.iconURL();
-        var boostCount = interaction.guild.premiumSubscriptionCount;
-        var boostTier = 0;
+  data: new SlashCommandBuilder()
+    .setName("serverinfo")
+    .setDescription("Receive information about the current guild"),
+  async execute(interaction) {
+    var serverIcon = interaction.guild.iconURL();
+    var boostCount = interaction.guild.premiumSubscriptionCount;
+    var boostTier = 0;
 
-        if(boostCount>=2) {
-            boostTier = 1;
-        } else if (boostCount >= 7) {
-            boostTier = 2;
-        } else if (boostCount >= 14) {
-            boostTier = 3;
-        }
-
-        const replyEmbed = new EmbedBuilder()
-            .setColor(0xff8355)
-            .setAuthor({ name: `${interaction.guild.name}`, iconURL: serverIcon })
-            .setThumbnail(serverIcon)
-            .addFields(
-                { name: 'General information', value: `
-                *Member count:* ${interaction.guild.memberCount}
-                *Boosts:* ${boostCount}
-                *Owner:* <@!${interaction.guild.ownerId}>`, inline: true 
-                },
-                { name: 'Other', value: `
-                *Boost tier:* ${boostTier}
-                *Region:* ${interaction.guild.region}`, inline: true 
-                },
-            )
-            .setFooter({ text: `${interaction.guild.id}` })
-            .setTimestamp();
-
-        interaction.reply({
-            embeds: [replyEmbed]
-        })
+    if (boostCount >= 2) {
+      boostTier = 1;
+    } else if (boostCount >= 7) {
+      boostTier = 2;
+    } else if (boostCount >= 14) {
+      boostTier = 3;
     }
-}
+
+    const replyEmbed = new EmbedBuilder()
+      .setColor(0xff8355)
+      .setAuthor({ name: `${interaction.guild.name}`, iconURL: serverIcon })
+      .setThumbnail(serverIcon)
+      .addFields(
+        {
+          name: "General information",
+          value: `
+          *Owner:* <@!${interaction.guild.ownerId}>
+                *Member count:* \`${interaction.guild.memberCount}\`
+                *Boosts:* \`${boostCount}\``,
+          inline: true,
+        },
+        {
+          name: "Other",
+          value: `
+          *Roles:* \`${interaction.guild.roles.cache.size - 1}\`
+                *Boost tier:* \`${boostTier}\`
+                *Channels:* \`${
+                  interaction.guild.channels.channelCountWithoutThreads
+                }\``,
+          inline: true,
+        }
+      )
+      .setFooter({ text: `${interaction.guild.id}` })
+      .setTimestamp();
+
+    interaction.reply({
+      embeds: [replyEmbed],
+    });
+  },
+};
