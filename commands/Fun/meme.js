76509ac9e613393e62cd9587ac2f0608c0bbbc9b
Toast <74485845+UndefinedToast@users.noreply.github.com>
2022-12-10 17:04:28 +0100

    Added meme command

---
+++ commands/Fun/meme.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { EmbedBuilder, PermissionFlagsBits } = require("discord.js");
+
+const requiredPerms = {
+  type: "flags",
+  key: [PermissionFlagsBits.SendMessages],
+};
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("meme")
+    .setDescription("Sends a random meme from r/memes"),
+  async execute(interaction) {
+    const data = await fetch(
+      `https://www.reddit.com/r/memes/random/.json`
+    ).then((res) => {
+      return res.json();
+    });
+
+    const title = data[0].data.children[0].data.title;
+    const image = data[0].data.children[0].data.url;
+    const author = data[0].data.children[0].data.author;
+
+    const replyEmbed = new EmbedBuilder()
+      .setTitle(title)
+      .setURL(`https://reddit.com/r/memes`)
+      .setImage(image)
+      .setFooter({ text: `Posted by u/${author}` })
+      .setColor(0x00ff00)
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [replyEmbed], ephemeral: true });
+  },
+  requiredPerms: requiredPerms,
+};
