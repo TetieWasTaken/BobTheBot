3e6963d7b5fd8c1dc542ffadb963b27d81583140
Toast <tjvssr@gmail.com>
2023-02-04 22:48:16 +0100

    ✨ feat(Ready): add client information

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,45 @@
 require("dotenv").config();
-const { REST, Routes } = require("discord.js");
-const { ActivityType } = require("discord.js");
+const { REST, Routes, ActivityType } = require("discord.js");
 const { logTimings } = require("../utils/logTimings");
+const { table } = require("table");
 
 module.exports = {
   name: "ready",
   once: true,
-  execute(client, commands) {
-    console.log("✅ BobTheBot is ready!");
+  async execute(client, commands) {
+    // This promise is required to make sure the WebSocket is fully ready before proceeding
+    await new Promise((resolve) => setTimeout(resolve, 5));
+
+    const config = {
+      header: {
+        alignment: "center",
+        content: "Client is ready!",
+      },
+    };
+
+    const WSStatus = {
+      0: "Ready",
+      1: "Connecting",
+      2: "Reconnecting",
+      3: "Idle",
+      4: "Nearly",
+      5: "Disconnected",
+      6: "Waiting for Guilds",
+      7: "Identifying",
+      8: "Resuming",
+    };
+
+    const cnslTable = [
+      ["WS Status", `${WSStatus[client.ws.status]}`],
+      ["Logged in as", `${client.user.tag}`],
+      ["ID", `${client.user.id}`],
+      ["Ping", `${await client.ws.ping}ms`],
+    ];
+
+    await console.log(
+      table(cnslTable, config),
+      "\n————————————————————————————————————————————————\n"
+    );
 
     const timerStart = Date.now();
 
