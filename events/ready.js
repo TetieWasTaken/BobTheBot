bdd13e5bc43bfb69df2d36da69d16d693cf53411
Toast <tjvssr@gmail.com>
2023-02-04 20:56:00 +0100

    🔨 refactor(Dependency): remove djs/rest and dapi

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
+const { REST, Routes } = require("discord.js");
 const { ActivityType } = require("discord.js");
 const { logTimings } = require("../utils/logTimings");
 
