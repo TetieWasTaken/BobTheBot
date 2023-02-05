eecfd6e9f61c0519760bf6da2bf1727394ba6c04
Toast <tjvssr@gmail.com>
2023-02-05 21:16:26 +0100

    🐛 fix(Changelog): fix branch protection

--- update_changelog.js
+++ update_changelog.js
@@ -22,39 +22,20 @@ if (
 
 const changelog = fs.readFileSync("CHANGELOG.md", "utf8");
 
-// Get everything inbetween <!--Changelog start--> and <!--Changelog end-->
 const changelogContent = changelog.match(
   /<!--Changelog start-->([\s\S]*)<!--Changelog end-->/m
 )[1];
-
-// Get the first line of the commit message
 let commitTitle = commitMsg.match(
   /^(revert: )?(\u00a9\s|\u00ae\s|[\u2000-\u3300]\s|\ud83c[\ud000-\udfff]\s|\ud83d[\ud000-\udfff]\s|\ud83e[\ud000-\udfff]\s)?\s*(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|types|wip)/
 );
 
 commitTitle = commitTitle[0].toUpperCase();
-
-// Get the commit description
 commitDescription = commitDescription.trim();
-
-// Get the files added
 filesAdded = filesAdded.trim().split(" ");
-
-// Get the files deleted
 filesDeleted = filesDeleted.trim().split(" ");
-
-// Get the files modified
 filesModified = filesModified.trim().split(" ");
-
-// Get the files renamed
 filesRenamed = filesRenamed.trim().split(" ");
-
-console.log(filesRenamed);
-
-// Get the date
 const date = new Date().toISOString().split("T")[0];
-
-// Create the new changelog entry
 const newChangelogEntry = `
 ### [ ${commitTitle} ] (${date})
 
@@ -90,8 +71,6 @@ ${
     : ""
 }
 `;
-
-// Create the new changelog
 const newChangelog = changelog.replace(
   /<!--Changelog start-->([\s\S]*)<!--Changelog end-->/m,
   `<!--Changelog start-->
@@ -100,6 +79,6 @@ ${changelogContent}
 <!--Changelog end-->`
 );
 
-// Write the new changelog to the file
-
 fs.writeFileSync("CHANGELOG.md", newChangelog);
+
+return console.log("Changelog updated successfully");
