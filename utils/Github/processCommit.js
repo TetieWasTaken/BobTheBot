978820c8e455fee5a23706c8133be50179dde301
Toast <tjvssr@gmail.com>
2023-02-06 20:16:42 +0100

    🚨 test(Workflows): test $GITHUB_OUTPUT

--- utils/Github/processCommit.js
+++ utils/Github/processCommit.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 const commitMsg = process.argv[2];
 const commitMsgRegex =
   /^(revert: )?(\u00a9\s|\u00ae\s|[\u2000-\u3300]\s|\ud83c[\ud000-\udfff]\s|\ud83d[\ud000-\udfff]\s|\ud83e[\ud000-\udfff]\s)?\s*(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|types|wip)(.+.+)?(!)?: .{1,72}/;
@@ -5,16 +7,17 @@ const commitMsgRegex =
 const importantCommitMsgRegex =
   /^(revert: )?(\u00a9\s|\u00ae\s|[\u2000-\u3300]\s|\ud83c[\ud000-\udfff]\s|\ud83d[\ud000-\udfff]\s|\ud83e[\ud000-\udfff]\s)?\s*(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|types|wip)(.+.+)?!: .{1,72}/;
 
+let output = "Failed";
+
 try {
   if (!commitMsg) {
-    return console.log("Failed");
   } else if (!commitMsg.match(commitMsgRegex)) {
-    return console.log("Invalid");
+    output = "Invalid";
   } else if (!commitMsg.match(importantCommitMsgRegex)) {
-    return console.log("Not important");
+    output = "Not important";
   } else {
-    return console.log("Important");
+    output = "Important";
   }
-} catch (e) {
-  console.log("Failed");
-}
+} catch (e) {}
+
+fs.writeFileSync(process.env.GITHUB_OUTPUT, `commit=${output}`);
