5b44f76cb4571e3d93e89cc97e3c3566b9ea58c3
Toast <tjvssr@gmail.com>
2023-02-12 17:14:34 +0100

    🔨 refactor(processComit): refactor regex

--- utils/Github/processCommit.js
+++ utils/Github/processCommit.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 const commitMsg = process.argv[2];
 const commitMsgRegex =
-  /^(revert: )?(\u00a9\s|\u00ae\s|[\u2000-\u3300]\s|\ud83c[\ud000-\udfff]\s|\ud83d[\ud000-\udfff]\s|\ud83e[\ud000-\udfff]\s)?\s*(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|types|wip)(.+.+)?(!)?: .{1,72}/;
+  /^^((revert: )?(\u00a9\s|\u00ae\s|[\u2000-\u3300]\s|\ud83c[\ud000-\udfff]\s|\ud83d[\ud000-\udfff]\s|\ud83e[\ud000-\udfff]\s)?\s*(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|types|wip)(.+.+)?(!)?: .{1,72}|Merge branch.*)/;
 
 const importantCommitMsgRegex =
   /^(revert: )?(\u00a9\s|\u00ae\s|[\u2000-\u3300]\s|\ud83c[\ud000-\udfff]\s|\ud83d[\ud000-\udfff]\s|\ud83e[\ud000-\udfff]\s)?\s*(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|types|wip)(.+.+)?!: .{1,72}/;
