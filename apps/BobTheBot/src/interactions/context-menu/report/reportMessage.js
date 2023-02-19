431aa8e60947aeca759febe16a979576afa67520
Toast <tjvssr@gmail.com>
2023-02-19 20:50:41 +0100

    feat: add report confirmation

--- apps/BobTheBot/src/interactions/context-menu/report/reportMessage.js
+++ apps/BobTheBot/src/interactions/context-menu/report/reportMessage.js
@@ -4,6 +4,8 @@ const {
   ModalBuilder,
   TextInputBuilder,
   TextInputStyle,
+  ButtonBuilder,
+  ButtonStyle,
   ActionRowBuilder,
   EmbedBuilder,
 } = require("discord.js");
@@ -32,74 +34,132 @@ module.exports = {
     interaction
       .awaitModalSubmit({ filter: (i) => i.customId === "report-message-modal", time: 5 * 60 * 1000 })
       .then(async (i) => {
-        const guildData = await GuildSchema.findOne({
-          GuildId: interaction.guild.id,
+        const message = i.fields.getTextInputValue("message-input");
+
+        const replyEmbed = new EmbedBuilder()
+          .setColor(0x2f3136)
+          .setAuthor({ name: `${interaction.user.tag}`, iconURL: `${interaction.member.displayAvatarURL()}` })
+          .setDescription(`${interaction.targetMessage.content}`)
+          .setFooter({ text: `#${interaction.channel.name}` })
+          .setTimestamp(interaction.targetMessage.createdAt);
+
+        const confirmButton = new ButtonBuilder()
+          .setCustomId("confirm-button")
+          .setLabel("Confirm")
+          .setStyle(ButtonStyle.Success);
+
+        const discordButton = new ButtonBuilder()
+          .setLabel("Report to Discord")
+          .setStyle(ButtonStyle.Link)
+          .setURL("https://dis.gd/report");
+
+        const actionRow = new ActionRowBuilder().addComponents(confirmButton, discordButton);
+        const confirmedRow = new ActionRowBuilder().addComponents(discordButton);
+
+        const reply = await i.reply({
+          content: `Are you sure you want to report this message?`,
+          embeds: [replyEmbed],
+          components: [actionRow],
+          ephemeral: true,
+          fetchReply: true,
         });
 
-        if (guildData && guildData.GuildLogChannel !== null) {
-          const reportChannel = interaction.guild.channels.cache.get(guildData.GuildLogChannel);
+        const confirmCollector = await reply.createMessageComponentCollector({
+          filter: (i) => i.customId === "confirm-button",
+          time: 60 * 1000,
+        });
 
-          const message = i.fields.getTextInputValue("message-input");
+        confirmCollector
+          .on("collect", async (i) => {
+            const guildData = await GuildSchema.findOne({
+              GuildId: interaction.guild.id,
+            });
 
-          const reportEmbed = new EmbedBuilder()
-            .setColor(0xff0000)
-            .setAuthor({ name: `${interaction.user.tag}`, iconURL: `${interaction.member.displayAvatarURL()}` })
-            .setTitle(`Message reported | ${interaction.channel.name}`)
-            .setURL(`${interaction.targetMessage.url}`)
-            .addFields(
-              {
-                name: `Message`,
-                value: `\`\`\`\n${interaction.targetMessage.content}\`\`\``,
-                inline: false,
-              },
-              {
-                name: `Target`,
-                value: `\`\`\`ini\nUser = ${interaction.targetMessage.author.tag}\nId = ${interaction.targetMessage.author.id}\`\`\``,
-                inline: true,
-              },
-              {
-                name: `Reason`,
-                value: `\`\`\`\n${message}\`\`\``,
-                inline: false,
+            if (guildData && guildData.GuildLogChannel !== null) {
+              const reportChannel = interaction.guild.channels.cache.get(guildData.GuildLogChannel);
+
+              const reportEmbed = new EmbedBuilder()
+                .setColor(0xff0000)
+                .setAuthor({ name: `${interaction.user.tag}`, iconURL: `${interaction.member.displayAvatarURL()}` })
+                .setTitle(`Message reported | ${interaction.channel.name}`)
+                .setURL(`${interaction.targetMessage.url}`)
+                .addFields(
+                  {
+                    name: `Message`,
+                    value: `\`\`\`\n${interaction.targetMessage.content}\`\`\``,
+                    inline: false,
+                  },
+                  {
+                    name: `Target`,
+                    value: `\`\`\`ini\nUser = ${interaction.targetMessage.author.tag}\nId = ${interaction.targetMessage.author.id}\`\`\``,
+                    inline: true,
+                  },
+                  {
+                    name: `Reason`,
+                    value: `\`\`\`\n${message}\`\`\``,
+                    inline: false,
+                  }
+                );
+
+              try {
+                reportChannel.send({ embeds: [reportEmbed] });
+              } catch (err) {
+                const replyEmbed = new EmbedBuilder()
+                  .setColor(0xff0000)
+                  .setTitle(`Report failed`)
+                  .setDescription(
+                    `I was unable to send the report to the report channel. Please check the permissions and try again.`
+                  );
+
+                return i.reply({ embeds: [replyEmbed], ephemeral: true });
               }
-            );
 
-          reportChannel.send({ embeds: [reportEmbed] });
+              const replyEmbed = new EmbedBuilder()
+                .setColor(0x57f287)
+                .setAuthor({
+                  name: `${interaction.targetMessage.author.tag} (${interaction.targetMessage.author.id})`,
+                  iconURL: `${interaction.targetMessage.user.displayAvatarURL()}`,
+                })
+                .setDescription(`${interaction.targetMessage.content}`)
+                .setFooter({ text: `#${interaction.channel.name}` })
+                .setTimestamp(interaction.targetMessage.createdAt);
 
-          const replyEmbed = new EmbedBuilder()
-            .setColor(0xffffff)
-            .setTitle(`Message reported`)
-            .setURL(`${interaction.targetMessage.url}`)
-            .addFields(
-              {
-                name: `Message`,
-                value: `\`\`\`\n${interaction.targetMessage.content}\`\`\``,
-                inline: false,
-              },
-              {
-                name: `Reason`,
-                value: `\`\`\`\n${message}\`\`\``,
-                inline: false,
-              }
-            );
-
-          i.reply({ embeds: [replyEmbed], ephemeral: true });
-        } else {
-          i.reply({ content: `The report channel has not been set up.`, ephemeral: true });
-        }
+              return i.update({
+                content: "Message reported successfully!",
+                embeds: [replyEmbed],
+                components: [confirmedRow],
+                ephemeral: true,
+              });
+            } else {
+              const replyEmbed = new EmbedBuilder()
+                .setColor(0x992d22)
+                .setTitle(`Report message failed`)
+                .setDescription(
+                  `The server moderators have not set up a log channel. Please contact them to set one up.`
+                );
+              return i.update({ embeds: [replyEmbed], components: [confirmedRow], ephemeral: true });
+            }
+          })
+          .on("end", () => {
+            const replyEmbed = new EmbedBuilder()
+              .setColor(0xff0000)
+              .setTitle(`Report message failed`)
+              .setDescription(`You took too long to respond. Please try again.`);
+            return interaction.followUp({ embeds: [replyEmbed], ephemeral: true });
+          });
       })
       .catch((err) => {
         if (err.message.endsWith("time")) {
           const replyEmbed = new EmbedBuilder()
             .setColor(0xff0000)
-            .setTitle(`Report message failed`)
+            .setTitle(`Report user failed`)
             .setDescription(`You took too long to respond. Please try again.`);
           interaction.followUp({ embeds: [replyEmbed], ephemeral: true });
         } else {
           console.log(err);
           const replyEmbed = new EmbedBuilder()
             .setColor(0xff0000)
-            .setTitle(`Report message failed`)
+            .setTitle(`Report user failed`)
             .setDescription(`An error occurred. Please try again.`);
           interaction.followUp({ embeds: [replyEmbed], ephemeral: true });
         }
--- apps/BobTheBot/src/interactions/context-menu/report/reportUser.js
+++ apps/BobTheBot/src/interactions/context-menu/report/reportUser.js
@@ -4,6 +4,8 @@ const {
   ModalBuilder,
   TextInputBuilder,
   TextInputStyle,
+  ButtonBuilder,
+  ButtonStyle,
   ActionRowBuilder,
   EmbedBuilder,
 } = require("discord.js");
@@ -32,67 +34,98 @@ module.exports = {
     interaction
       .awaitModalSubmit({ filter: (i) => i.customId === "report-user-modal", time: 5 * 60 * 1000 })
       .then(async (i) => {
-        const guildData = await GuildSchema.findOne({
-          GuildId: interaction.guild.id,
+        const reason = i.fields.getTextInputValue("reason-input");
+
+        const replyEmbed = new EmbedBuilder()
+          .setColor(0x2f3136)
+          .setAuthor({ name: `${interaction.user.tag}`, iconURL: `${interaction.member.displayAvatarURL()}` })
+          .setFooter({ text: `#${interaction.channel.name}` })
+          .setTimestamp(interaction.targetUser.createdAt);
+
+        const confirmButton = new ButtonBuilder()
+          .setCustomId("confirm-button")
+          .setLabel("Confirm")
+          .setStyle(ButtonStyle.Success);
+
+        const discordButton = new ButtonBuilder()
+          .setLabel("Report to Discord")
+          .setStyle(ButtonStyle.Link)
+          .setURL("https://dis.gd/report");
+
+        const actionRow = new ActionRowBuilder().addComponents(confirmButton, discordButton);
+        const confirmedRow = new ActionRowBuilder().addComponents(discordButton);
+
+        const reply = await i.reply({
+          content: `Are you sure you want to report this message?`,
+          embeds: [replyEmbed],
+          components: [actionRow],
+          ephemeral: true,
+          fetchReply: true,
         });
 
-        if (guildData && guildData.GuildLogChannel !== null) {
-          const reportChannel = interaction.guild.channels.cache.get(guildData.GuildLogChannel);
+        const confirmCollector = await reply.createMessageComponentCollector({
+          filter: (i) => i.customId === "confirm-button",
+          time: 60 * 1000,
+        });
 
-          const reason = i.fields.getTextInputValue("reason-input");
+        confirmCollector.on("collect", async (i) => {
+          const guildData = await GuildSchema.findOne({
+            GuildId: interaction.guild.id,
+          });
 
-          const reportEmbed = new EmbedBuilder()
-            .setColor(0xff0000)
-            .setAuthor({ name: `${interaction.user.tag}`, iconURL: `${interaction.member.displayAvatarURL()}` })
-            .setTitle(`User reported | ${interaction.channel.name}`)
-            .setURL(`https://discord.com/users/${interaction.targetUser.id}`)
-            .addFields(
-              {
-                name: `Target`,
-                value: `\`\`\`ini\nUser = ${interaction.targetUser.tag}\nId = ${interaction.targetUser.id}\`\`\``,
-                inline: true,
-              },
-              {
-                name: `Reason`,
-                value: `\`\`\`\n${reason}\`\`\``,
-                inline: false,
-              }
-            );
+          if (guildData && guildData.GuildLogChannel !== null) {
+            const reportChannel = interaction.guild.channels.cache.get(guildData.GuildLogChannel);
 
-          try {
-            reportChannel.send({ embeds: [reportEmbed] });
-          } catch (err) {
-            const replyEmbed = new EmbedBuilder()
+            const reportEmbed = new EmbedBuilder()
               .setColor(0xff0000)
-              .setTitle(`Report failed`)
-              .setDescription(
-                `I was unable to send the report to the report channel. Please check the permissions and try again.`
+              .setAuthor({ name: `${interaction.user.tag}`, iconURL: `${interaction.member.displayAvatarURL()}` })
+              .setTitle(`User reported | ${interaction.channel.name}`)
+              .setURL(`https://discord.com/users/${interaction.targetUser.id}`)
+              .addFields(
+                {
+                  name: `Target`,
+                  value: `\`\`\`ini\nUser = ${interaction.targetUser.tag}\nId = ${interaction.targetUser.id}\`\`\``,
+                  inline: true,
+                },
+                {
+                  name: `Reason`,
+                  value: `\`\`\`\n${reason}\`\`\``,
+                  inline: false,
+                }
               );
 
-            return i.reply({ embeds: [replyEmbed], ephemeral: true });
+            try {
+              reportChannel.send({ embeds: [reportEmbed] });
+            } catch (err) {
+              const replyEmbed = new EmbedBuilder()
+                .setColor(0xff0000)
+                .setTitle(`Report failed`)
+                .setDescription(
+                  `I was unable to send the report to the report channel. Please check the permissions and try again.`
+                );
+
+              return i.reply({ embeds: [replyEmbed], ephemeral: true });
+            }
+
+            const replyEmbed = new EmbedBuilder()
+              .setColor(0x57f287)
+              .setAuthor({
+                name: `${interaction.targetUser.tag} (${interaction.targetUser.id})`,
+                iconURL: `${interaction.targetUser.displayAvatarURL()}`,
+              })
+              .setFooter({ text: `#${interaction.channel.name}` })
+              .setTimestamp(interaction.targetUser.createdAt);
+
+            return i.update({
+              content: "User reported successfully!",
+              embeds: [replyEmbed],
+              components: [confirmedRow],
+              ephemeral: true,
+            });
+          } else {
+            i.reply({ content: `The report channel has not been set up.`, ephemeral: true });
           }
-
-          const replyEmbed = new EmbedBuilder()
-            .setColor(0xffffff)
-            .setTitle(`User reported`)
-            .setURL(`https://discord.com/users/${interaction.targetUser.id}`)
-            .addFields(
-              {
-                name: `Target`,
-                value: `\`\`\`ini\nUser = ${interaction.targetUser.tag}\nId = ${interaction.targetUser.id}\`\`\``,
-                inline: true,
-              },
-              {
-                name: `Reason`,
-                value: `\`\`\`\n${reason}\`\`\``,
-                inline: false,
-              }
-            );
-
-          i.reply({ embeds: [replyEmbed], ephemeral: true });
-        } else {
-          i.reply({ content: `The report channel has not been set up.`, ephemeral: true });
-        }
+        });
       })
       .catch((err) => {
         if (err.message.endsWith("time")) {
