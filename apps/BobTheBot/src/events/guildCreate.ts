7635cb08e0b6a05274a8a5d01432d7f495818730
Toast <tjvssr@gmail.com>
2023-02-20 16:47:56 +0100

    fix: fix guild type

--- apps/BobTheBot/src/events/guildCreate.ts
+++ apps/BobTheBot/src/events/guildCreate.ts
@@ -1,7 +1,9 @@
+import type { Guild } from "discord.js";
+
 module.exports = {
   name: "guildCreate",
   once: false,
-  async execute(guild: any) {
-    guild.members.me.setNickname("Bob");
+  async execute(guild: Guild) {
+    guild.members.me?.setNickname("Bob");
   },
 };
