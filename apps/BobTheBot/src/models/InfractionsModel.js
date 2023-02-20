62d67dccd4d2dd44254f232a36c3b45286849678
Toast <tjvssr@gmail.com>
2023-02-20 21:26:45 +0100

    chore: refactor infractionsmodel to ts

--- apps/BobTheBot/src/models/InfractionsModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const profileSchema = new mongoose.Schema({
-  GuildId: String,
-  UserId: String,
-  Punishments: Array,
-});
-
-module.exports = mongoose.model("InfractionsModel", profileSchema);
