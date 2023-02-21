79b5501df370ef588758e638df38b98527bfeafe
Toast <tjvssr@gmail.com>
2023-02-21 20:35:38 +0100

    refactor: convert convertMS to ts

--- apps/BobTheBot/src/utils/convertMS.ts
+++ apps/BobTheBot/src/utils/convertMS.ts
@@ -1,18 +1,9 @@
-function padTo2Digits(num: string | number) {
+function padTo2Digits(num: string | number): string {
   return num.toString().padStart(2, "0");
 }
-module.exports = {
-  convertMS: (milliseconds: number) => {
-    let seconds = Math.floor(milliseconds / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
 
-    seconds = seconds % 60;
-    minutes = minutes % 60;
-    hours = hours % 24;
-
-    return `\`${padTo2Digits(hours)}\` hours, \`${padTo2Digits(minutes)}\` minutes, \`${padTo2Digits(
-      seconds
-    )}\` seconds`;
-  },
-};
+export function convertMS(milliseconds: number): string {
+  return `\`${padTo2Digits(Math.floor(milliseconds / 3600000))}\` hours, \`${padTo2Digits(
+    Math.floor((milliseconds / 60000) % 60)
+  )}\` minutes, \`${padTo2Digits(Math.floor((milliseconds / 1000) % 60))}\` seconds`;
+}
