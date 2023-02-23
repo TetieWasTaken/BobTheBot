66d0fab862dd2acdeee2aae2f4bd59841a969167
Toast <tjvssr@gmail.com>
2023-02-23 20:55:16 +0100

    refactor: convert rank.js to ts

--- apps/BobTheBot/src/interactions/Information/rank.ts
+++ apps/BobTheBot/src/interactions/Information/rank.ts
@@ -1,14 +1,14 @@
-const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
-const LevelSchema = require("../../models/LevelModel");
+import { SlashCommandBuilder, EmbedBuilder, ChatInputCommandInteraction } from "discord.js";
+import { LevelModel } from "../../models/index.js";
 
 const requiredBotPerms = {
-  type: "flags",
-  key: [],
+  type: "flags" as const,
+  key: [] as const,
 };
 
 const requiredUserPerms = {
-  type: "flags",
-  key: [],
+  type: "flags" as const,
+  key: [] as const,
 };
 
 module.exports = {
@@ -16,7 +16,7 @@ module.exports = {
     .setName("rank")
     .setDescription("Returns xp and rank of given user in the server")
     .addUserOption((option) => option.setName("target").setDescription("User to display rank of").setRequired(false)),
-  async execute(interaction) {
+  async execute(interaction: ChatInputCommandInteraction) {
     let user = interaction.options.getUser("target") ?? interaction.user;
 
     if (user.bot) {
@@ -26,7 +26,9 @@ module.exports = {
       });
     }
 
-    let data = await LevelSchema.findOne({
+    if (!interaction.guild) return interaction.reply({ content: "Something went wrong!", ephemeral: true });
+
+    let data = await LevelModel.findOne({
       GuildId: interaction.guild.id,
       UserId: user.id,
     });
@@ -34,22 +36,17 @@ module.exports = {
     let userXP = 0;
     let userLevel = 0;
 
-    if (data) {
+    if (data && data.UserXP && data.UserLevel) {
       userXP = data.UserXP;
       userLevel = data.UserLevel;
-    }
-
-    let userNickname = ` (${user.nickname ?? ""})`;
-    if (userNickname == " ()") {
-      userNickname = "";
     }
 
     let xpNeeded = 50 * (userLevel + 1) ** 2 + 50;
 
     const rankEmbed = new EmbedBuilder()
-      .setColor(interaction.guild.members.me.displayHexColor)
+      .setColor(interaction.guild?.members?.me?.displayHexColor ?? 0x5865f2)
       .setAuthor({
-        name: `${user.username}#${user.discriminator}` + userNickname,
+        name: `${user.tag} (${user.id})`,
         iconURL: `${user.displayAvatarURL()}`,
       })
       .addFields(
@@ -62,7 +59,7 @@ module.exports = {
       )
       .setTimestamp();
 
-    interaction.reply({
+    return interaction.reply({
       embeds: [rankEmbed],
     });
   },
