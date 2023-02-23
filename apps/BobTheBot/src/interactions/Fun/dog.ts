ae4b41ae61db98019e62ab3787a7da575556080b
Toast <tjvssr@gmail.com>
2023-02-23 11:08:51 +0100

    refactor: convert dog.js to ts

--- apps/BobTheBot/src/interactions/Fun/dog.ts
+++ apps/BobTheBot/src/interactions/Fun/dog.ts
@@ -1,23 +1,23 @@
-const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+import { ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder } from "discord.js";
 
 const requiredBotPerms = {
-  type: "flags",
-  key: [],
+  type: "flags" as const,
+  key: [] as const,
 };
 
 const requiredUserPerms = {
-  type: "flags",
-  key: [],
+  type: "flags" as const,
+  key: [] as const,
 };
 
 module.exports = {
   data: new SlashCommandBuilder().setName("woof").setDescription("Get a random dog image!"),
-  async execute(interaction) {
+  async execute(interaction: ChatInputCommandInteraction) {
     const res = await fetch("https:random.dog/woof.json").then((res) => res.json());
     const embed = new EmbedBuilder()
       .setTitle("Woof!")
       .setImage(res.url)
-      .setColor(interaction.guild.members.me.displayHexColor)
+      .setColor(interaction.guild?.members?.me?.displayHexColor ?? 0x5865f2)
       .setTimestamp()
       .setFooter({
         text: `Requested by ${interaction.user.tag}`,
