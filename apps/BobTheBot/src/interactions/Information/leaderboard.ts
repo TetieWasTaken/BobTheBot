6073fc776b1009f09abb5f0bdb2a4a3ac8c4d4c3
Toast <tjvssr@gmail.com>
2023-02-23 18:10:32 +0100

    refactor: convert leaderboard.js to ts

--- apps/BobTheBot/src/interactions/Information/leaderboard.ts
+++ apps/BobTheBot/src/interactions/Information/leaderboard.ts
@@ -1,23 +1,22 @@
-const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
-
-const LevelSchema = require("../../models/LevelModel");
+import { SlashCommandBuilder, EmbedBuilder, ChatInputCommandInteraction } from "discord.js";
+import { LevelModel } from "../../models/index.js";
 
 const requiredBotPerms = {
-  type: "flags",
-  key: [],
+  type: "flags" as const,
+  key: [] as const,
 };
 
 const requiredUserPerms = {
-  type: "flags",
-  key: [],
+  type: "flags" as const,
+  key: [] as const,
 };
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("leaderboard")
     .setDescription("Returns the top 10 users with the most amount of XP"),
-  async execute(interaction) {
-    const data = await LevelSchema.find({ GuildID: interaction.guild.id }).sort({ UserXP: -1 }).limit(10);
+  async execute(interaction: ChatInputCommandInteraction<"cached">) {
+    const data = await LevelModel.find({ GuildID: interaction.guild.id }).sort({ UserXP: -1 }).limit(10);
 
     const mappedData = data
       .map((d, i) => `\`${i + 1}\` <@${d.UserId}> - Level: \`${d.UserLevel}\` - XP: \`${d.UserXP}\``)
@@ -26,7 +25,7 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setTitle("Leaderboard")
       .setDescription(mappedData.toString())
-      .setColor(interaction.guild.members.me.displayHexColor);
+      .setColor(interaction.guild?.members?.me?.displayHexColor ?? 0x5865f2);
     interaction.reply({ embeds: [embed] });
   },
   requiredBotPerms: requiredBotPerms,
