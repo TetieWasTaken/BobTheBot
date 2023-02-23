3fc59eb852338f73f03c304296b0b3342f2d19a4
Toast <tjvssr@gmail.com>
2023-02-23 21:16:47 +0100

    refactor: convert uptime.js to ts

---
+++ apps/BobTheBot/src/interactions/Information/uptime.ts
@@ -0,0 +1,27 @@
+import { SlashCommandBuilder, EmbedBuilder, ChatInputCommandInteraction } from "discord.js";
+import { convertMS } from "../../utils/index.js";
+
+const requiredBotPerms = {
+  type: "flags" as const,
+  key: [] as const,
+};
+
+const requiredUserPerms = {
+  type: "flags" as const,
+  key: [] as const,
+};
+
+module.exports = {
+  data: new SlashCommandBuilder().setName("uptime").setDescription("Receive the bots's uptime"),
+  async execute(interaction: ChatInputCommandInteraction<"cached">) {
+    const embed = new EmbedBuilder()
+      .setTitle("⏱️ Uptime")
+      .setDescription(`${convertMS(interaction.client.uptime)}`)
+      .setColor(interaction.guild?.members?.me?.displayHexColor ?? 0x5865f2)
+      .setFooter({ text: `${interaction.client.uptime}ms` });
+
+    return await interaction.reply({ embeds: [embed] });
+  },
+  requiredBotPerms: requiredBotPerms,
+  requiredUserPerms: requiredUserPerms,
+};
