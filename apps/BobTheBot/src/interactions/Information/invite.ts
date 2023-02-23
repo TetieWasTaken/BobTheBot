316f3b7bc3cf0f9a2e159fe4101acdf999f63d05
Toast <tjvssr@gmail.com>
2023-02-23 17:11:58 +0100

    refactor: convert invite.js to ts

--- apps/BobTheBot/src/interactions/Information/invite.ts
+++ apps/BobTheBot/src/interactions/Information/invite.ts
@@ -1,24 +1,26 @@
-const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+import type { ChatInputCommandInteraction } from "discord.js";
+
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 const requiredBotPerms = {
-  type: "flags",
-  key: [],
+  type: "flags" as const,
+  key: [] as const,
 };
 
 const requiredUserPerms = {
-  type: "flags",
-  key: [],
+  type: "flags" as const,
+  key: [] as const,
 };
 
 module.exports = {
   data: new SlashCommandBuilder().setName("invite").setDescription("Receive an invite link for the bot"),
-  async execute(interaction) {
+  async execute(interaction: ChatInputCommandInteraction) {
     const replyEmbed = new EmbedBuilder()
       .setTitle("Invite BobTheBot")
       .setDescription(
         "[**Click here to invite BobTheBot to your server**](https://discord.com/api/oauth2/authorize?client_id=1036359071508484237&permissions=8&scope=bot%20applications.commands)"
       )
-      .setColor(interaction.guild.members.me.displayHexColor)
+      .setColor(interaction.guild?.members?.me?.displayHexColor ?? 0x5865f2)
       .setTimestamp();
     interaction.reply({
       embeds: [replyEmbed],
