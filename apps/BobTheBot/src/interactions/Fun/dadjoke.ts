e07d7814887500dfde7d510b06c94970778af1c8
Toast <tjvssr@gmail.com>
2023-02-23 09:28:08 +0100

    refactor: convert dadjoke to ts

--- apps/BobTheBot/src/interactions/Fun/dadjoke.ts
+++ apps/BobTheBot/src/interactions/Fun/dadjoke.ts
@@ -1,23 +1,23 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
-const fs = require("fs");
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import fs from "fs";
 
 const requiredBotPerms = {
-  type: "flags",
-  key: [],
+  type: "flags" as const,
+  key: [] as const,
 };
 
 const requiredUserPerms = {
-  type: "flags",
-  key: [],
+  type: "flags" as const,
+  key: [] as const,
 };
 
 module.exports = {
   data: new SlashCommandBuilder().setName("dadjoke").setDescription("Receive a random dad joke!"),
-  async execute(interaction) {
+  async execute(interaction: ChatInputCommandInteraction) {
     const dadJokesTxt = fs.readFileSync("./resources/dadjokes.txt").toString().split("\n");
     const randomNum = Math.floor(Math.random() * 710);
 
-    interaction.reply({ content: dadJokesTxt[randomNum], ephemeral: true });
+    interaction.reply({ content: dadJokesTxt[randomNum] });
   },
   requiredBotPerms: requiredBotPerms,
   requiredUserPerms: requiredUserPerms,
