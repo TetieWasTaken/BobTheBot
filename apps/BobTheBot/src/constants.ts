317d1f443993c638eada5e5b251bd566fe6717f6
Toast <tjvssr@gmail.com>
2023-02-28 10:49:09 +0100

    feat: add constants

---
+++ apps/BobTheBot/src/constants.ts
@@ -0,0 +1,8 @@
+export const enum Color {
+  DiscordEmbedBackground = 0x2f3136, // jet/dark grey
+  DiscordSuccess = 0x57f287, // green
+  DiscordDanger = 0xed4245, // red
+  DiscordWarning = 0xfee75c, // yellow
+  DiscordPrimary = 0x5865f2, // blurple
+  DiscordGem = 0xeb459e, // fuchsia
+}
