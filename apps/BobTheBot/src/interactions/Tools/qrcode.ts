fd5e520c3922b3d5847aa2f27db1cb6451789ae4
Toast <tjvssr@gmail.com>
2023-03-01 20:29:11 +0100

    refactor: convert qrcode.js to ts

--- apps/BobTheBot/src/interactions/Tools/qrcode.ts
+++ apps/BobTheBot/src/interactions/Tools/qrcode.ts
@@ -1,13 +1,14 @@
-const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+import { SlashCommandBuilder, EmbedBuilder, ChatInputCommandInteraction } from "discord.js";
+import { Color } from "../../constants";
 
 const requiredBotPerms = {
-  type: "flags",
-  key: [],
+  type: "flags" as const,
+  key: [] as const,
 };
 
 const requiredUserPerms = {
-  type: "flags",
-  key: [],
+  type: "flags" as const,
+  key: [] as const,
 };
 
 module.exports = {
@@ -22,8 +23,8 @@ module.exports = {
         .setMaxLength(2000)
     ),
   cooldownTime: 10 * 1000,
-  async execute(interaction) {
-    const text = interaction.options.getString("url");
+  async execute(interaction: ChatInputCommandInteraction<"cached">) {
+    const text = interaction.options.getString("url", true);
     const baseURL = "http://api.qrserver.com/v1";
     const regex = /[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
 
@@ -38,7 +39,7 @@ module.exports = {
     const encodedURL = `${baseURL}/create-qr-code/?size=150x150&data=` + encodeURIComponent(text);
 
     const replyEmbed = new EmbedBuilder()
-      .setColor("ffffff")
+      .setColor(Color.DiscordPrimary)
       .setTitle(`QR Code for ${text.replace("https://", "").replace("http://", "")}`)
       .setImage(encodedURL)
       .setFooter({ text: `Try it out, it works!` })
