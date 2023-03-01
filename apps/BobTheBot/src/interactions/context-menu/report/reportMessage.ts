a788332962ade9a5372d60296bb923cfb20e3e2a
Toast <tjvssr@gmail.com>
2023-03-01 22:07:49 +0100

    refactor: convert reportMessage.js to ts

--- apps/BobTheBot/src/interactions/context-menu/report/reportMessage.ts
+++ apps/BobTheBot/src/interactions/context-menu/report/reportMessage.ts
@@ -1,4 +1,4 @@
-const {
+import {
   ContextMenuCommandBuilder,
   ApplicationCommandType,
   ModalBuilder,
@@ -8,12 +8,14 @@ const {
   ButtonStyle,
   ActionRowBuilder,
   EmbedBuilder,
-} = require("discord.js");
-const GuildSchema = require("../../../models/GuildModel");
+  MessageContextMenuCommandInteraction,
+} from "discord.js";
+import { GuildModel } from "../../../models/index.js";
+import { Color } from "../../../constants.js";
 
 module.exports = {
   data: new ContextMenuCommandBuilder().setName("Report Message").setType(ApplicationCommandType.Message),
-  execute(interaction) {
+  execute(interaction: MessageContextMenuCommandInteraction<"cached">) {
     const modal = new ModalBuilder().setCustomId("report-message-modal").setTitle("Report Message");
 
     const messageInput = new TextInputBuilder()
@@ -25,7 +27,7 @@ module.exports = {
       .setRequired(true)
       .setStyle(TextInputStyle.Paragraph);
 
-    const firstActionRow = new ActionRowBuilder().addComponents(messageInput);
+    const firstActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(messageInput);
 
     modal.addComponents(firstActionRow);
 
@@ -34,10 +36,12 @@ module.exports = {
     interaction
       .awaitModalSubmit({ filter: (i) => i.customId === "report-message-modal", time: 5 * 60 * 1000 })
       .then(async (i) => {
+        if (!interaction.channel) return;
+
         const message = i.fields.getTextInputValue("message-input");
 
         const replyEmbed = new EmbedBuilder()
-          .setColor(0x2f3136)
+          .setColor(Color.DiscordEmbedBackground)
           .setAuthor({ name: `${interaction.user.tag}`, iconURL: `${interaction.member.displayAvatarURL()}` })
           .setDescription(
             `${interaction.targetMessage.content.length > 0 ? interaction.targetMessage.content : "No content"}`
@@ -55,8 +59,8 @@ module.exports = {
           .setStyle(ButtonStyle.Link)
           .setURL("https://dis.gd/report");
 
-        const actionRow = new ActionRowBuilder().addComponents(confirmButton, discordButton);
-        const confirmedRow = new ActionRowBuilder().addComponents(discordButton);
+        const actionRow = new ActionRowBuilder<ButtonBuilder>().addComponents(confirmButton, discordButton);
+        const confirmedRow = new ActionRowBuilder<ButtonBuilder>().addComponents(discordButton);
 
         const reply = await i.reply({
           content: `Are you sure you want to report this message?`,
@@ -73,15 +77,15 @@ module.exports = {
 
         confirmCollector
           .on("collect", async (i) => {
-            const guildData = await GuildSchema.findOne({
+            if (!interaction.channel) return;
+
+            const guildData = await GuildModel.findOne({
               GuildId: interaction.guild.id,
             });
 
-            if (guildData && guildData.GuildLogChannel !== null) {
-              const reportChannel = interaction.guild.channels.cache.get(guildData.GuildLogChannel);
-
+            if (guildData && guildData.GuildLogChannel) {
               const reportEmbed = new EmbedBuilder()
-                .setColor(0xed4245)
+                .setColor(Color.DiscordDanger)
                 .setAuthor({ name: `${interaction.user.tag}`, iconURL: `${interaction.member.displayAvatarURL()}` })
                 .setTitle(`Message reported | ${interaction.channel.name}`)
                 .setURL(`${interaction.targetMessage.url}`)
@@ -103,21 +107,33 @@ module.exports = {
                   }
                 );
 
+              const reportChannel = interaction.guild.channels.cache.get(guildData.GuildLogChannel);
+
+              if (!reportChannel || !reportChannel.isTextBased()) {
+                const replyEmbed = new EmbedBuilder()
+                  .setColor(Color.DiscordDanger)
+                  .setTitle(`Report failed`)
+                  .setDescription(
+                    `I was unable to find the log channel. Make sure I can view and send messages in it.`
+                  );
+                return void i.reply({ embeds: [replyEmbed], ephemeral: true });
+              }
+
               try {
                 reportChannel.send({ embeds: [reportEmbed] });
               } catch (err) {
                 const replyEmbed = new EmbedBuilder()
-                  .setColor(0xed4245)
+                  .setColor(Color.DiscordDanger)
                   .setTitle(`Report failed`)
                   .setDescription(
                     `I was unable to send the report to the report channel. Please check the permissions and try again.`
                   );
 
-                return i.reply({ embeds: [replyEmbed], ephemeral: true });
+                return void i.reply({ embeds: [replyEmbed], ephemeral: true });
               }
 
               const replyEmbed = new EmbedBuilder()
-                .setColor(0x57f287)
+                .setColor(Color.DiscordSuccess)
                 .setAuthor({
                   name: `${interaction.targetMessage.author.tag} (${interaction.targetMessage.author.id})`,
                   iconURL: `${interaction.targetMessage.author.displayAvatarURL()}`,
@@ -128,44 +144,43 @@ module.exports = {
                 .setFooter({ text: `#${interaction.channel.name}` })
                 .setTimestamp(interaction.targetMessage.createdAt);
 
-              return i.update({
+              return void i.update({
                 content: "Message reported successfully!",
                 embeds: [replyEmbed],
                 components: [confirmedRow],
-                ephemeral: true,
               });
             } else {
               const replyEmbed = new EmbedBuilder()
-                .setColor(0x992d22)
+                .setColor(Color.DiscordDanger)
                 .setTitle(`Report message failed`)
                 .setDescription(
                   `The server moderators have not set up a log channel. Please contact them to set one up.`
                 );
-              return i.update({ embeds: [replyEmbed], components: [confirmedRow], ephemeral: true });
+              return void i.update({ embeds: [replyEmbed], components: [confirmedRow] });
             }
           })
           .on("end", () => {
             const replyEmbed = new EmbedBuilder()
-              .setColor(0xed4245)
+              .setColor(Color.DiscordDanger)
               .setTitle(`Report message failed`)
               .setDescription(`You took too long to respond. Please try again.`);
-            return interaction.followUp({ embeds: [replyEmbed], ephemeral: true });
+            return void interaction.followUp({ embeds: [replyEmbed] });
           });
       })
       .catch((err) => {
         if (err.message.endsWith("time")) {
           const replyEmbed = new EmbedBuilder()
-            .setColor(0xed4245)
+            .setColor(Color.DiscordDanger)
             .setTitle(`Report user failed`)
             .setDescription(`You took too long to respond. Please try again.`);
-          interaction.followUp({ embeds: [replyEmbed], ephemeral: true });
+          return interaction.followUp({ embeds: [replyEmbed], ephemeral: true });
         } else {
           console.log(err);
           const replyEmbed = new EmbedBuilder()
-            .setColor(0xed4245)
+            .setColor(Color.DiscordDanger)
             .setTitle(`Report user failed`)
             .setDescription(`An error occurred. Please try again.`);
-          interaction.followUp({ embeds: [replyEmbed], ephemeral: true });
+          return interaction.followUp({ embeds: [replyEmbed], ephemeral: true });
         }
       });
   },
