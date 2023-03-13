d428b8d9959ac43129dd2845c8835d4be3a9e0e5
TetieWasTaken <tjvssr@gmail.com>
2023-03-13 21:11:19 +0100

    chore: refactor deploy

--- apps/BobTheBot/src/deploy.ts
+++ apps/BobTheBot/src/deploy.ts
@@ -1,7 +1,7 @@
-import fs from "node:fs";
 import process from "node:process";
 import { REST, Routes } from "discord.js";
 import dotenv from "dotenv";
+import { TestCommand } from "./interactions/index.js";
 import { logger } from "./utils/index.js";
 
 dotenv.config();
@@ -9,37 +9,6 @@ dotenv.config();
 if (!process.env.BOT_TOKEN) {
   logger.error("No bot token provided.");
   process.exit(1);
-}
-
-const commandFolders = fs.readdirSync("./src/interactions/").filter((item: string) => !/(?:^|\/)\.[^./]/g.test(item));
-const interactions = [];
-
-/* eslint-disable @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports */
-
-for (const folder of commandFolders) {
-  const commandFiles = fs.readdirSync(`./src/interactions/${folder}`);
-  for (const file of commandFiles) {
-    if (!file.endsWith(".js") && !file.endsWith(".ts")) {
-      const subFiles = fs.readdirSync(`./src/interactions/${folder}/${file}`);
-      for (const subFile of subFiles) {
-        try {
-          const command = require(`./interactions/${folder}/${file}/${subFile.replace(".ts", ".js")}`);
-          interactions.push(command.data.toJSON());
-        } catch (error) {
-          logger.error(error);
-        }
-      }
-
-      continue;
-    }
-
-    try {
-      const command = require(`./interactions/${folder}/${file.replace(".ts", ".js")}`);
-      interactions.push(command.data.toJSON());
-    } catch (error) {
-      logger.error(error);
-    }
-  }
 }
 
 const rest = new REST({
@@ -54,7 +23,7 @@ const rest = new REST({
     }
 
     if (process.env.ENV === "production") {
-      await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: interactions });
+      await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [TestCommand] });
     } else {
       if (!process.env.GUILD_ID) {
         logger.error("No guild id provided.");
@@ -62,12 +31,12 @@ const rest = new REST({
       }
 
       await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {
-        body: interactions,
+        body: [TestCommand],
       });
     }
   } catch (error) {
     logger.error(error);
   }
 
-  logger.info(`Successfully registered ${interactions.length} application commands.`);
+  logger.info(`Successfully registered application commands.`);
 })();
